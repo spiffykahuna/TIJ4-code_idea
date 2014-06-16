d7cab8b0c17924c335ba5b1a42e2b5a6e0d265c3
Denis Konstantinov <denis.konstantinov@gmail.com>
2014-06-16 17:40:42 +0300

    concurency/exercise1[23]

---
+++ concurrency/exercises/Exercise12.java
@@ -0,0 +1,36 @@
+package concurrency.exercises;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Exercise12 implements Runnable {
+    private int i = 0;
+
+    public synchronized int getValue() {
+        return i;
+    }
+
+    private synchronized void evenIncrement() {
+        i++;
+        i++;
+    }
+
+    @Override
+    public void run() {
+        while (true)
+            evenIncrement();
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Exercise12 at = new Exercise12();
+        exec.execute(at);
+        while (true) {
+            int val = at.getValue();
+            if (val % 2 != 0) {
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+}
